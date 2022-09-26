2bc2bb3f99e30055df546b629c2e1e16d4e08d4b
Lucas Collado <43991862+7BSmurf@users.noreply.github.com>
2022-09-26 13:36:12 +0200

    Update walker.hpp

--- turtlebot_walker/include/walker.hpp
+++ turtlebot_walker/include/walker.hpp
@@ -6,26 +6,16 @@
 #include <sensor_msgs/LaserScan.h>
 
 namespace sdw {
-/**
- * @brief Walker class
- * **/
+
 class Walker {
  private:
-    /**
-     * @brief Stores distance (in meters) from obstacle.
-     * **/
+
     double distance;
  public:
-    /**
-     * @brief Walker class constructor.
-     * @param node ROS Nodehandle
-     * **/
+
     Walker(ros::NodeHandle node);
     
-    /**
-     * @brief Laserscan subscriber callback
-     * @param data Single scan from a planar laser range finder
-     * **/
+
     void laserCallback(const sensor_msgs::LaserScan::ConstPtr& data);
 };
 } // namespace sdw
